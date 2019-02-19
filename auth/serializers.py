a77c60d15290e84c35f95bbd02cc17416c70c30d
crycetruly <crycetruly@gmail.com>
2019-02-19 17:22:40 +0300

    Registered a user

--- auth/serializers.py
+++ auth/serializers.py
@@ -2,19 +2,42 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=['username','id','first_name','last_name','username','email','password']
+        fields=('id','username','email')
 
-        extra_kwargs={
-            'password':{'write_only':True}
+
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering new users.
+    """
+    email = serializers.EmailField()
+
+    class Meta(object):
+        extra_kwargs = {
+            "password": {
+                "style": {"input_type": "password"},
+                "write_only": True,
+            }
         }
+        fields = ('username', "email", "password","first_name","last_name")
+        model = User
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(**validated_data)
+        user.is_active = False
+        user.first_name = validated_data['first_name']
+        user.last_name = validated_data['last_name']
+        user.save()
+        return user
 
 
-        def create(self,validated_data):
-            user=User.objects.create_user(**validated_data)
-            user.set_password([validated_data['password']])
-            return user
+
 
 
--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import GenericAPIView
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,UserSerializer
 from rest_framework.response import Response
 
 # Create your views here.
@@ -7,9 +7,12 @@ class RegisterApi(GenericAPIView):
     serializer_class=RegisterSerializer
 
     def post(self,request,*args,**kwags):
-        serializer=self.get_serializer(data=request.data)
+        serializer=self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response('user created successfully')
+        user=serializer.save()
+        return Response({
+            "user":UserSerializer(user, context=self.get_serializer_context()).data
+        })
+        
 
   
