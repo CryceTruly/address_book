677a436afc0a3482840a98666cecea976c70b1d4
crycetruly <crycetruly@gmail.com>
2019-02-20 09:21:24 +0300

    Protected profile route

--- auth/urls.py
+++ auth/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   path('api/auth', include('knox.urls')),
   path('api/auth/register', RegisterAPI.as_view()),
   path('api/auth/login', LoginAPI.as_view()),
-  path('api/auth/user', UserAPI.as_view()),
+  path('api/auth/profile', UserAPI.as_view()),
   path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
